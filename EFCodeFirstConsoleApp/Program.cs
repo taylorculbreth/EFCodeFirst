00612d885b7e753d5c5127f98578ca08b338fa9f
taylorculbreth <tsc@taylorculbreth.com>
2015-11-06 22:12:22 -0600

    EF code first

---
+++ EFCodeFirstConsoleApp/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFCodeFirstConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var ctx = new SchoolContext())
+            {
+                Student stud = new Student()
+                {
+                    StudentName = "new student"
+                };
+                ctx.Students.Add(stud);
+                ctx.SaveChanges();
+            }
+        }
+    }
+}
---
+++ EFCodeFirstConsoleApp/Student.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFCodeFirstConsoleApp
+{
+    public class Student
+    {
+        public Student()
+        {
+
+        }
+        public int StudentId { get; set; }
+        public string StudentName { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public byte[] Photo { get; set; }
+        public decimal Height { get; set; }
+        public float Weight { get; set; }
+        public Standard Standard { get; set; }
+    }
+}
